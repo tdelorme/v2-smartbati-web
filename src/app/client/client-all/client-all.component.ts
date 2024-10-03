dc70438beb70db1501fd445ad3cca6f104489555
Thibaut DELORME <thibaut.delorme@outlook.fr>
2024-10-03 23:08:01 +0200

    feat(client_all): add table to show all client of current user

--- src/app/client/client-all/client-all.component.ts
+++ src/app/client/client-all/client-all.component.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { Client } from '../model/client.model';
+import { catchError, map, merge, of, startWith, switchMap } from 'rxjs';
+import { ClientService } from '../client.service';
 
 @Component({
   selector: 'app-client-all',
   standalone: true,
-  imports: [],
+  imports: [MatProgressSpinnerModule, MatTableModule, MatSortModule, MatPaginatorModule],
   templateUrl: './client-all.component.html',
   styleUrl: './client-all.component.scss'
 })
-export class ClientAllComponent {
+export class ClientAllComponent implements AfterViewInit{
+
+  displayedColumns: string[] = ['lastName', 'firstName', 'email', 'phone', 'address'];
+  data: Client[] = [];
+  resultsLength = 0;
+  pageSize = 10;
+  isLoadingResults = true;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(private clientService: ClientService) {}
+
+  ngAfterViewInit(): void {
+      this.paginator.page.pipe(
+        startWith({}),
+        switchMap(() => {
+          this.isLoadingResults = true;
+          return this.clientService.getByPage(this.paginator.pageIndex, this.pageSize).pipe(catchError(() => of(null)));
+        }),
+        map(data => {
+          this.isLoadingResults = false;
+
+          if (data === null) {
+            return [];
+          }
+
+          this.resultsLength = data.total_count;
+          return data.data;
+        })
+      )
+      .subscribe(data => this.data = data);
+  }
 
 }
--- src/app/client/client.service.ts
+++ src/app/client/client.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Client } from './model/client.model';
 import { Observable } from 'rxjs';
-import { LocalService } from '../shared/local.storage.service';
+import { PageableApiResponse, ApiResponse } from '../shared/model/api.response.model';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +12,11 @@ export class ClientService {
   constructor(private http: HttpClient
   ) { }
 
-  public create(client: Client): Observable<Client> {
-    return this.http.post<Client>('/api/client/create', client);
+  public create(client: Client): Observable<ApiResponse<Client>> {
+    return this.http.post<ApiResponse<Client>>('/api/client/create', client);
+  }
+
+  public getByPage(index: number, pageSize: number): Observable<PageableApiResponse<Client[]>> {
+    return this.http.get<PageableApiResponse<Client[]>>(`/api/client/page/${index}/size/${pageSize}`);
   }
 }
