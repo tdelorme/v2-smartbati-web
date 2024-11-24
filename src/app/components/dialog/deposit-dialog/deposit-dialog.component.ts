6934f93b102438e7d3f277c81ebf0364cc8d7ef3
Thibaut DELORME <thibaut.delorme@outlook.fr>
2024-11-24 10:25:31 +0100

    feat(deposit_dialog): missing dialog

---
+++ src/app/components/dialog/deposit-dialog/deposit-dialog.component.ts
@@ -0,0 +1,32 @@
+import { Component, inject, model } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MAT_DIALOG_DATA, MatDialogActions, MatDialogClose, MatDialogContent, MatDialogRef, MatDialogTitle } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+@Component({
+  selector: 'app-deposit-dialog',
+  standalone: true,
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    MatDialogTitle,
+    MatDialogContent,
+    MatDialogActions,
+    MatDialogClose
+  ],
+  templateUrl: './deposit-dialog.component.html',
+  styleUrl: './deposit-dialog.component.scss'
+})
+export class DepositDialogComponent {
+  readonly dialogRef = inject(MatDialogRef<DepositDialogComponent>);
+  readonly data = inject<number>(MAT_DIALOG_DATA);
+  readonly deposit = model(this.data);
+
+  cancel(): void {
+    this.dialogRef.close();
+  }
+}
